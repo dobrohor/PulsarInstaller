495c62bf8daddbc6d46eaff4db895c5dbb359605
Volodymyr Dobrohorskyi <dobrohorskyi.v@gmail.com>
2026-03-04 14:27:48 +0100

    Creation of creation of example list
    PS: Water is watery

--- App.xaml.cs
+++ App.xaml.cs
@@ -3,10 +3,18 @@ using System.Data;
 using System.Windows;
 
 namespace PulsarInstaller;
+using Classes;
 
 /// <summary>
 /// Interaction logic for App.xaml
+/// Core class. Gets imported list -> executes tasks.
+/// All tasks must be run as separate threads, waiting for prerequisites to be met before executing.
+/// TODO: Add "smart" system to shuffle tasks if one depends on other
 /// </summary>
 public partial class App : Application
 {
+    public App()
+    {
+        ListIO.CreateList("C:\\Users\\Work\\Desktop\\list.json");
+    }
 }
---
+++ Classes/ListIO.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using PulsarInstaller.Enums;
+
+namespace PulsarInstaller.Classes;
+
+// For creation/reading list.json
+public class ListIO 
+{
+    public static List<Task>? taskList {get; set;}
+    
+    public static void CreateList(string path)
+    {
+        // Create list.json with example task
+        Task exampleTask1 = 
+            new Task("Example Task1", TaskType.Copy, "C:\\source", 
+                new List<string>{"C:\\destination"}, "This is an example task.");
+        Prerequisite examplePrerequisite1 = 
+            new Prerequisite("Example Prerequisite1", Enums.PrerequisiteType.TaskCompleted, 
+                new List<string>{"Example Task1"}, "Proceed only if Example Task1 is completed.");
+        Task exampleTask2 = 
+            new Task("Example Task2", TaskType.Install, "C:\\installer.exe", 
+                new List<string>{"C:\\install"}, "This is another example task.", new List<Prerequisite>{examplePrerequisite1});
+        taskList = new List<Task>();
+        taskList.Add(exampleTask1);
+        taskList.Add(exampleTask2);
+        
+        // Configure JsonSerializer options to convert enums to strings and format nicely
+        var options = new System.Text.Json.JsonSerializerOptions
+        {
+            WriteIndented = true,
+            PropertyNamingPolicy = System.Text.Json.JsonNamingPolicy.CamelCase,
+            Converters = { new System.Text.Json.Serialization.JsonStringEnumConverter() }
+        };
+        
+        string json = System.Text.Json.JsonSerializer.Serialize(taskList, options);
+        File.WriteAllText(path, json);
+    }
+}
